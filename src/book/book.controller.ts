ab50c9a0dfe4a427f4c65b3fdb90ded0f2a79e83
RUDRANSHFLY <mayamayavir@gmail.com>
2025-08-12 12:26:57 +0530

    feat : add curd to the book done

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { BookService } from './book.service';
 import { GetUser } from 'src/auth/decorators';
-import { CreateBookDto, GetBookDto } from './dto';
+import { CreateBookDto, EditBookDto, GetBookDto } from './dto';
+
 
 
 @UseGuards(AuthGuard)
@@ -22,5 +23,30 @@ export class BookController {
     @Get()
     getBook(@Query() query : GetBookDto){
         return this.bookService.getBook(query)
+    }
+
+    @Get(':id')
+    getBookById(
+        @Param('id') bookId : string,
+    ){
+        return this.bookService.getBookById(bookId)
+    }
+
+    @Patch(':id')
+    updateBookById(
+        @Param('id') bookId : string,
+        @GetUser('sub') userId : string,
+        @Body() dto : EditBookDto,
+    ){
+        return this.bookService.updateBookById(userId,bookId,dto)
+    }
+
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @Delete(':id')
+    deleteBookById(
+        @Param('id') bookId : string,
+        @GetUser('sub') userId : string,
+    ){
+        return this.bookService.deleteBookById(userId,bookId)
     }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,7 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateBookDto, GetBookDto } from './dto';
 import { Prisma } from '@prisma/client';
 import { DbService } from 'src/db/db.service';
+import { EditBookDto } from './dto/edit-book.dto';
 
 @Injectable()
 export class BookService {
@@ -9,7 +10,7 @@ export class BookService {
         private readonly prisma: DbService
     ) { }
 
-    
+
     async createBook(userId: string, dto: CreateBookDto) {
         try {
             const newBook = await this.prisma.book.create({
@@ -34,13 +35,13 @@ export class BookService {
 
     async getBook(query: GetBookDto) {
         try {
-            let { limit , page } = query;
+            let { limit, page } = query;
 
             //? if no pagination or query specified then return all books
-            if(!('page' in query) || !('limit' in query)){
+            if (!query.limit || !query.page) {
                 return await this.prisma.book.findMany({
-                    orderBy : {
-                        createdAt : "desc"
+                    orderBy: {
+                        createdAt: "desc"
                     }
                 })
             }
@@ -54,9 +55,9 @@ export class BookService {
             const [books, total] = await Promise.all([
                 this.prisma.book.findMany({
                     skip,
-                    take : limit,
-                    orderBy : {
-                        createdAt : "desc"
+                    take: limit,
+                    orderBy: {
+                        createdAt: "desc"
                     }
                 }),
                 this.prisma.book.count()
@@ -66,11 +67,76 @@ export class BookService {
                 total,
                 page,
                 limit,
-                totalPages : Math.ceil(total/limit),
+                totalPages: Math.ceil(total / limit),
                 books
             }
         } catch (error) {
             console.error(error)
+            throw error;
+        }
+    }
+
+    async getBookById(bookId: string) {
+        try {
+            return this.prisma.book.findUnique({
+                where: {
+                    id: bookId
+                }
+            })
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async updateBookById(userId: string, bookId: string, dto: EditBookDto) {
+        try {
+            const book = await this.prisma.book.findUnique({
+                where: {
+                    id: bookId,
+                }
+            })
+
+            //! User have created book have privaleged to edit the book
+            if (!book || book.userId !== userId) {
+                throw new ForbiddenException('Access to resource denied')
+            }
+
+            return await this.prisma.book.update({
+                where: {
+                    id: bookId,
+                },
+                data: {
+                    ...dto,
+                }
+            })
+        } catch (error) {
+            console.log(error);
+            throw error
+        }
+    }
+
+    async deleteBookById(userId: string, bookId: string) {
+        try {
+            const book = await this.prisma.book.findUnique({
+                where: {
+                    id: bookId,
+                }
+            })
+
+            //! User have created book have privaleged to delete the book
+            if (!book || book.userId !== userId) {
+                throw new ForbiddenException('Access to resource denied')
+            }
+
+            return await this.prisma.book.delete({
+                where: {
+                    id: bookId,
+                    userId,
+                }
+            })
+        } catch (error) {
+            console.log(error);
             throw error;
         }
     }
