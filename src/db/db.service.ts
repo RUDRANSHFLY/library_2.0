34ddab1f6dff56c28bfe721999412b3aae2849c3
RUDRANSHFLY <mayamayavir@gmail.com>
2025-08-12 08:12:01 +0530

    feat : Add prisma Service

---
+++ src/db/db.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+
+@Injectable()
+export class DbService extends PrismaClient {
+    constructor(config: ConfigService) {
+        super({
+            datasources: {
+                db: {
+                    url: config.get('DATABASE_URL'),
+                }
+            }
+        })
+    }
+
+    cleanDb(){
+        return this.$transaction([
+            this.user.deleteMany(),
+            this.book.deleteMany()
+        ])
+    }
+
+
+}
