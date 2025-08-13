27fef76da5849d62b747ffb524fda7f629bea18a
RUDRANSHFLY <mayamayavir@gmail.com>
2025-08-13 11:38:33 +0530

    feat : Add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Library API')
+  .setDescription('a library management api')
+  .setVersion('1.0')
+  .addTag('books')
+  .build();   
+  const documentFactory = () => SwaggerModule.createDocument(app,config)
+  SwaggerModule.setup('api',app,documentFactory)
+
+  app.enableCors()
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist : true,
     transform : true,
   }))
+  
   await app.listen(process.env.PORT ?? 3000);
 }
+
 void bootstrap();
